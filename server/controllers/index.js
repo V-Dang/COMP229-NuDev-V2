ff80452c981f2e4de807406149c17b720ad52b9d
V-Dang <vivian.dang96@hotmail.com>
2022-12-11 21:23:44 -0500

    database switched

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -130,7 +130,7 @@ module.exports.processRegisterPage = (req, res, next) => {
             //redirect the user and authenticate them
 
             return passport.authenticate('local')(req, res, () => {
-                res.redirect('/contact-list')
+                res.redirect('/')
             });
         }
     });
