a8d08cc1cecbeb54fc1389474e228bae944ac207
V-Dang <vivian.dang96@hotmail.com>
2022-12-11 20:01:45 -0500

    database switched

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@
 //Must decalre URI
 module.exports = {
         //numbers found on listening terminal mongod
-    "URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
+    //"URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
     //URI:"mongodb://127.0.0.1:27017/business_contacts",
  
-    //"URI":"mongodb+srv://Hussein:YBxK9qpVPFqAwqLM@dbserver.atdtgw9.mongodb.net/survey"
+    "URI":"mongodb+srv://Hussein:YBxK9qpVPFqAwqLM@dbserver.atdtgw9.mongodb.net/survey"
     //user:user1
     //pw:LryS3WGEtxzjtFN9
 };
